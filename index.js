008058e351e3973a19973ace33695dd7fa063597
Harshit Sharma <harshit19csu411@ncuindia.edu>
2022-04-16 01:17:10 +0530

    Added simple routes

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 
 const app = express();
+var cors = require("cors");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cors());
 
 const port = process.env.PORT || 8080;
 
@@ -9,5 +15,11 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  res.json({ status: "running" });
+  res.json({ status: "sup" });
+});
+
+app.post("/runCode", (req, res) => {
+  const some = req.body;
+
+  res.json({ language: `${some.language}`, code: `${some.code}` });
 });
