5747f61e616d71e8a91765e66eea2a492929bc7e
Gopal <gopalsays108@gmail.com>
2022-04-18 01:46:09 +0530

    file generated succesfully

---
+++ generateFile.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const dirCodes = path.join(__dirname, "codes");
+
+
+if(!fs.existsSync(dirCodes)){
+    fs.mkdirSync(dirCodes,{recursive : true});
+}
+
+const generateFile = async (format, code) => {
+    const jobId = "GingerPen"
+    const fileName  = `${jobId}.${format}`
+    const filepath = path.join(dirCodes,fileName);
+    await fs.writeFileSync(filepath,code);
+    return filepath;
+};
+
+module.exports = {
+    generateFile,
+}
